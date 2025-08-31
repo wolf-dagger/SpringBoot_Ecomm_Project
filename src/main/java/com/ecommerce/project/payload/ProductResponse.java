0ecac064067e4c3e8031979fcb9e1e014f06b850
wolf-dagger <73078569+wolf-dagger@users.noreply.github.com>
2025-08-31 23:24:03 +0530

    Product initial setup done

--- src/main/java/com/ecommerce/project/payload/ProductResponse.java
+++ src/main/java/com/ecommerce/project/payload/ProductResponse.java
@@ -1,16 +1,14 @@
 package com.ecommerce.project.payload;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class CategoryDTO {
-
-    private Long categoryId;
-    private String categoryName;
-
+public class ProductResponse {
+    private List<ProductDTO> content;
 }
